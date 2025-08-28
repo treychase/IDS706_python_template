776ccd84899d5faf36496bdfef4feb34e7051eb2
Trey Chase <trchase7@yahoo.com>
2025-08-27 20:50:44 -0400

    initial python script, python tests

---
+++ hello.py
@@ -0,0 +1,8 @@
+def introduce_me(name: str) -> str:
+    """Introduce myself to my new classmates in IDS 706."""
+    return f"Hello, {name}, my name is Trey, your new classmate!"
+
+
+def abs_diff(a: int, b: int) -> int:
+    """Return the absolute value of the difference between two integers"""
+    return abs(a - b)
---
+++ test_hello.py
@@ -0,0 +1,12 @@
+from hello import abs_diff, introduce_me
+
+def test_introduce_me():
+    assert(
+        introduce_me("Tully")
+        == "Hello, Tully, my name is Trey, your new classmate!"
+    )
+
+def test_abs_diff():
+    assert(
+        abs_diff(2,3) == 1
+    )
