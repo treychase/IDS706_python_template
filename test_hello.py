d3cbd742c53595f4a60eda77d39e444da3bafc85
Trey Chase <trchase7@yahoo.com>
2025-08-28 01:04:10 +0000

    modiy test

--- test_hello.py
+++ test_hello.py
@@ -1,12 +1,9 @@
 from hello import abs_diff, introduce_me
 
+
 def test_introduce_me():
-    assert(
-        introduce_me("Tully")
-        == "Hello, Tully, my name is Trey, your new classmate!"
-    )
+    assert introduce_me("Tully") == "Hello, Tully, my name is Trey, your new classmate!"
+
 
 def test_abs_diff():
-    assert(
-        abs_diff(2,3) == 1
-    )
+    assert abs_diff(2, 3) == 1
